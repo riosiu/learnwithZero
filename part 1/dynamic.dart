c2f698fd4cb0f6e7f453bc77e08c24f56661fdaa
richard <richardo.aog@gmail.com>
2022-07-15 14:22:58 +0700

    nambah lagi

---
+++ part 1/dynamic.dart
@@ -0,0 +1,13 @@
+/**
+   * Dynamic sendiri digunakan untuk bisa menampung semua jenis tipe data
+   */
+void main() {
+  dynamic variable = 100;
+  print(variable);
+
+  variable = true;
+  print(variable);
+
+  variable = 'Richard';
+  print(variable);
+}
--- part 1/string.dart
+++ part 1/string.dart
@@ -1,1 +1,44 @@
-void main() {}
+void main() {
+  String firstName = 'Richard';
+  String lastName = 'Sinulingga';
+
+  print(firstName);
+  print(lastName);
+
+  /**
+   * dapat menggunakan firnat ${ekspresion} ketika kita ingin
+   * mengambik data dari variabel lain
+   */
+  var fullName = '$firstName ${lastName}';
+  print(fullName);
+
+  /**
+   * Dapat juga menggunakan karakter backslash untuk
+   * menginisialisasikan string
+   */
+
+  var text = 'this is \'dart\' \$cool';
+  print(text);
+
+  /**
+   * Menggabungkan string menggunakan (+) atau menggunakan enter/tab
+   */
+
+  var name1 = firstName + " " + lastName;
+  var name2 = 'Richard' 'Sinulingga';
+
+  print(name1);
+  print(name2);
+
+  /**
+   * Multiline string ketika menginisialisasikan string yang panjang
+   * dengan menggunakan petik satu atau petik dua.
+   */
+
+  var longString = '''
+this is long string
+multiline string at 
+learning dart
+''';
+  print(longString);
+}
